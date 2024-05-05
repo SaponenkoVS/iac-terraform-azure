bddd6284b9e5f47a438ec6df0d33daf8cb81b340
SaponenkoVS <saponenkovladislav2001@mail.ru>
2024-05-05 13:56:37 +0200

    init blob strg

--- azure-blob-storage/provider.tf
+++ azure-blob-storage/provider.tf
@@ -1,4 +1,19 @@
 provider "azurerm" {
   version = ">= 0.11"
+  skip_provider_registration = true
   features {}
+}
+
+terraform {
+  backend "local" {
+    path = "./terraform.tfstate"
+  }
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
 }
